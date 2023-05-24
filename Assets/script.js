f7e2c772b77f285dbf8efa9316568d591a2e78ab
Freebird <bo.salinas12@gmail.com>
2023-05-24 10:36:55 -0500

    updates to get help

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,54 @@
-var getUserCity = function (user) {
-    var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&8a12f98687e75d009acb2c76284f6f41'
-  
+var searchBtnEl = document.querySelector('#search-btn')
+//for when user clicks on search button
+function handleSearchBtnClick() {
+    //fetch weather for city in input
+
+    //diplay weather
+    var cityName = 'Houston' //TODO: get city name from input 
+    fetchWeather()//pass in city name using input
+
+    //save city name in local storage
+};
+//for when user clicks on a button in recent history
+function handleCityBtnClick() {
+    //fetch weather for city in ibtn
+
+    //diplay weather
+    fetchWeather(cityName)//pass in city name using which history button that is clicked
+};
+
+searchBtnEl.addEventListener('click', handleSearchBtnClick);
+localStorage.setItem(id, description);
+
+//takes in city name and returns city data
+function fetchWeather(cityName) {
+    console.log('searching weather for', cityName);
+    // use API 
+    var apiUrl = `api.openweathermap.org/data/2.5/forecast?q=${city name}&appid=8a12f98687e75d009acb2c76284f6f41`
+
     fetch(apiUrl)
-      .then(function (response) {
-        if (response.ok) {
-          response.json().then(function (data) {
-            display(data, user);
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      })
-      .catch(function (error) {
-        alert('Unable to connect to Open Weather Map');
-      });
-  };
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(data){ };
+            //get data we actually need
+        var mappedForecastData = 
+        var mappedData = data.list.map(weather)
+            return {
+            renderCurrentWeather();
+            renderForecast();
+        };
+        )
+
+};
+console.log(mappedData);
+
+var displayCurrentWeather = document.querySelector('#results'); 
+
+function renderCurrentWeather(mappedForecastData) {
+
+};
+
+function renderForecast(mappedData) {
+
+};
